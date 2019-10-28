4cd4c0c585a4a8f4c76da4246f05cf11b9fabcaf
PriscillaRoiz <priscillagarcia02@hotmail.com>
2019-10-28 13:43:12 -0200

    Problemas resolvidos

--- 00a_revisao/index.js
+++ 00a_revisao/index.js
@@ -5,3 +5,23 @@
 // [] escutar o clique do botão
 // [] criar função de alterar a cor usando rgb()
 // [] usar Math.random() e Math.floor() ou parseInt()
+
+const body = document.body;
+const botao = document.getElementById("trocaCor");
+
+body.style.backgroundColor = "purple";
+
+botao.addEventListener('click', mudaCor)
+function geraInteiro(){
+    const n = parseInt(Math.random() * 255);
+    return n; 
+}
+function mudaCor(){
+const r = parseInt(Math.random() * 255);
+const g = parseInt(Math.random() * 255);
+const b = parseInt(Math.random() * 255);
+
+body.style.backgroundColor = "rgb(" + r + ", "+ g + ", "+ b +")"
+console.log("rgb(" + r + ", "+ g + ", "+ b +")");
+console.log(r,g,b)
+}
--- 00b_revisao/index.js
+++ 00b_revisao/index.js
@@ -6,6 +6,28 @@
 // [] criar função de alterar a cor usando rgb()
 // [] usar Math.random() e Math.floor() ou parseInt()
 
+const body = document.body;
+const botao = document.getElementById("trocaCor");
+
+body.style.backgroundColor = "purple";
+botao.addEventListener('click', () => {
+    const r = geraInteiro(255);
+    const g = geraInteiro(255);
+    const b = geraInteiro(255);
+
+    body.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")"
+    console.log("rgb(" + r + ", " + g + ", " + b + ")");
+    console.log(r, g, b)
+})
+
+
+// Arrow function =>, metodo mais pratico de criar funcao.
+// sempre colocar = depois do nome da function
+// pode ser sem parenteses se for um unico parametro
+
+geraInteiro = num => parseInt(Math.random() * num);
+
+
 // ------------------------------------------------------------------------
 // 2. melhorando o código
 // [] remover repetição de código
